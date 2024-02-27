3ea8c9f5244749ea003d0053afa6539efc822ed7
Anunay Argha <anunayargha@gmail.com>
2024-02-27 21:39:06 +0600

    project load done

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
 "use strict";
 
-const loa
+const loadPhone = async () => {
+	const url = "https://openapi.programming-hero.com/api/phones?search=iphone";
+	const response = await fetch(url);
+	const json = await response.json();
+	const data = json.data;
+	const cardContainer = document.getElementById("card_container");
+
+	data.forEach((phone) => {
+		const cardHtml = `
+    <div
+					class="card"
+				>
+					<figure class="card_image">
+						<img
+							src=${phone.image}
+							alt="Iphone"
+							class="rounded-lg"
+						/>
+					</figure>
+					<div class="px-7 space-y-2">
+						<h2 class="text-2xl font-bold mt-6">${phone.phone_name}</h2>
+						<p class="text-lg leading-relaxed">
+							There are many variations of passages of available, but the
+							majority have suffered.
+						</p>
+						<p class="text-2xl font-bold">$999</p>
+						<button
+							class="card_button"
+						>
+							Show Details
+						</button>
+					</div>
+				</div>
+    `;
+		cardContainer.insertAdjacentHTML("afterbegin", cardHtml);
+	});
+};
+
+loadPhone();
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export const content = ["./*.{html,js}"];
 export const theme = {
-	extend: {},
+	extend: {
+		colors: {
+			primary: "#0D6EFD0D",
+			button: "#0D6EFD",
+			dark1: '#100F0F',
+			dark2: "#403F3F",
+		},
+	},
+
+	fontFamily: {
+		poppin: '"Poppins", sans-serif',
+	},
 };
 export const plugins = [require("daisyui")];

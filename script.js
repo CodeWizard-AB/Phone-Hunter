91614c11a1b32acb218ec7bc8c21e17adc837171
Anunay Argha <anunayargha@gmail.com>
2024-02-27 23:08:50 +0600

    phone updated loading done

--- script.js
+++ script.js
@@ -5,38 +5,42 @@
 const cardContainer = document.getElementById("card_container");
 const searchBtn = document.getElementById("search_btn");
 const searchInput = document.getElementById("search_input");
+const showAllBtn = document.getElementById("show_btn");
 
 // * EVENT FUNCTIONS -
 
-const loadPhone = async (search) => {
+const loadPhone = async (search, clicked) => {
 	const url = `https://openapi.programming-hero.com/api/phones?search=${search}`;
 	const response = await fetch(url);
 	const json = await response.json();
 	const data = json.data;
+	const indexLast = clicked ? -1 : 6;
 	cardContainer.innerHTML = "";
+	showAllBtn.classList.add("hidden");
 
-	data.slice(0, 9).forEach((phone) => {
+	data.slice(0, indexLast).forEach((phone) => {
 		const cardHtml = `
     <div class="card">
 			<figure class="card_image">
-				<img
-					src=${phone.image}
+      <img
+      src=${phone.image}
 					alt="Iphone"
 					class="rounded-lg"
-				/>
+          />
 			</figure>
 			<div class="px-7 space-y-2">
 				<h2 class="text-2xl font-bold mt-6">${phone.phone_name}</h2>
 				<p class="text-lg leading-relaxed">
 					There are many variations of passages of available, but the
 					majority have suffered.
-				</p>
+          </p>
 				<p class="text-2xl font-bold">$999</p>
 				<button class="card_button">Show Details</button>
 			</div>
 		</div>
     `;
 		cardContainer.insertAdjacentHTML("afterbegin", cardHtml);
+		if (data.length >= 5) showAllBtn.classList.remove("hidden");
 	});
 };
 
@@ -47,4 +51,9 @@ loadPhone();
 searchBtn.addEventListener("click", (e) => {
 	e.preventDefault();
 	loadPhone(searchInput.value);
+});
+
+showAllBtn.addEventListener("click", (e) => {
+	e.preventDefault();
+	loadPhone(searchInput.value, true);
 });
